35815d016f50bf967ec0d2f8fc2771ac6081771b
Ermin <usb007agent@gmail.com>
2016-12-14 18:27:08 +0100

    Code improvements

--- wikipedia-viewer/script.js
+++ wikipedia-viewer/script.js
@@ -9,14 +9,14 @@ $(document).ready(function () {
       async: false,
       dataType: "json",
       success: function (data) {
+        $("#results").html("");
         for (var i = 0; i < data[1].length; i++) {
-          
+          $("#results").prepend("<li><a href="+data[3][i]+">"+data[1][i]+"</a><p>"+data[2][i]+"</p></li>");
         }
       },
       error: function (error) {
         alert("Error! Please contact me so i could be able to fix error.");
       }
     });
-
   });
 });
