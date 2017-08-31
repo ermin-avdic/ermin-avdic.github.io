57ccb06d22323edc8e4eca7f4fa1a9576c47a20b
Ermin <usb007agent@gmail.com>
2017-09-01 01:17:10 +0200

    Improved animations
    Animations now start by scroll.

--- Portfolio/assets/js/script.js
+++ Portfolio/assets/js/script.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
   $(".button-collapse").sideNav();
-  window.randomize = function() {
-	$('#htmlProgress').attr('data-progress', 95);
-  $('#cssProgress').attr('data-progress', 90);
-  $('#javascriptProgress').attr('data-progress', 90);
-  $('#nodejsProgress').attr('data-progress', 85);
-}
-  setTimeout(window.randomize, 200);
+
+  $(document).on('scroll', function() {
+    if($(this).scrollTop()>=$('#scrollDetect').position().top){
+      animate();
+    }
+});
+
+  function animate(){
+    window.randomize = function() {
+  	$('#htmlProgress').attr('data-progress', 95);
+    $('#cssProgress').attr('data-progress', 90);
+    $('#javascriptProgress').attr('data-progress', 90);
+    $('#nodejsProgress').attr('data-progress', 85);
+  }
+    setTimeout(window.randomize, 200);
+  }
 });
