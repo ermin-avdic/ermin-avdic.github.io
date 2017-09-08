28eae3bc3c36f546e7ece248b0e2031f24773ab3
Ermin <usb007agent@gmail.com>
2017-09-08 23:18:24 +0200

    Implemented basic calculator functionality

--- Javascript Calculator/script.js
+++ Javascript Calculator/script.js
@@ -1,6 +1,8 @@
 var input = document.querySelector("input");
-var digits = document.querySelectorAll(".digits");
+var digits = document.querySelectorAll(".digits, .operator");
 var ac = document.querySelector("#ac")
+var square = document.querySelector("#square");
+var equals = document.querySelector("#equals");
 var backspace = document.querySelector("#backspace");
 
 for (var i = 0; i < digits.length; i++) {
@@ -8,3 +10,20 @@ for (var i = 0; i < digits.length; i++) {
     input.value += this.innerHTML;
   });
 }
+
+ac.addEventListener("click", function() {
+  input.value = "";
+});
+
+square.addEventListener("click", function() {
+  input.value *= input.value;
+});
+
+backspace.addEventListener("click", function() {
+  input.value = input.value.slice(0, -1)
+});
+
+equals.addEventListener("click", function() {
+  var res = eval(input.value);
+  input.value = res;
+});
