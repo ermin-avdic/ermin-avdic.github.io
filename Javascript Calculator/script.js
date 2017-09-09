f54872bcb85cc0afd71ee4e413991507b20dbeed
Ermin <usb007agent@gmail.com>
2017-09-09 23:59:17 +0200

    Implemented new method

--- Javascript Calculator/script.js
+++ Javascript Calculator/script.js
@@ -1,29 +1,28 @@
 var input = document.querySelector("input");
-var digits = document.querySelectorAll(".digits, .operator");
+var span = document.querySelectorAll("span");
+var digits = document.querySelectorAll(".digits");
+var operators = document.querySelectorAll(".operator");
 var ac = document.querySelector("#ac")
 var square = document.querySelector("#square");
 var equals = document.querySelector("#equals");
 var backspace = document.querySelector("#backspace");
+var op = ["+", "*", "/"];
 
-for (var i = 0; i < digits.length; i++) {
-  digits[i].addEventListener("click", function() {
-    input.value += this.innerHTML;
-  });
+for (var i = 0; i < span.length; i++) {
+  span[i].onclick = function() {
+    var value = this.innerHTML;
+
+    if (value === "AC") {
+      input.value = "";
+    } else if (value === "^2") {
+      input.value *= input.value;
+    } else if (value === '<i class="fa fa-arrow-left" aria-hidden="true"></i>') {
+      input.value = input.value.slice(0, -1);
+    } else if (value === "=") {
+      input.value = eval(input.value);
+    } else {
+      var lastChar = input.value[input.value.length - 1];
+    }
+
+  }
 }
-
-ac.addEventListener("click", function() {
-  input.value = "";
-});
-
-square.addEventListener("click", function() {
-  input.value *= input.value;
-});
-
-backspace.addEventListener("click", function() {
-  input.value = input.value.slice(0, -1)
-});
-
-equals.addEventListener("click", function() {
-  var res = eval(input.value);
-  input.value = res;
-});
