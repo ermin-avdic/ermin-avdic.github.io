ec68e986a34cab8dff8cae66254e8eaf80ae683b
Ermin <usb007agent@gmail.com>
2017-09-10 22:54:31 +0200

    Few improvements
    Not working properly yet, but its close

--- Javascript Calculator/script.js
+++ Javascript Calculator/script.js
@@ -20,9 +20,17 @@ for (var i = 0; i < span.length; i++) {
       input.value = input.value.slice(0, -1);
     } else if (value === "=") {
       input.value = eval(input.value);
-    } else {
+    } else if (op.indexOf(value) > -1) {
       var lastChar = input.value[input.value.length - 1];
+      if (input.value != "" && op.indexOf(lastChar) == -1) {
+        input.value += value;
+      } else if (input.value == "" && value == "-") {
+        input.value += value;
+      } else if (op.indexOf(lastChar) > -1 && input.value.length > 1) {
+        input.value = input.value.replace(/$/, value);
+      }
+    } else {
+      input.value += value;
     }
-
   }
 }
