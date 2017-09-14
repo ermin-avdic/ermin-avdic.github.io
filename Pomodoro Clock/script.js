8f4a68d4a9def083793ec219b4199886205cd15e
Ermin <usb007agent@gmail.com>
2017-09-14 20:47:51 +0200

    Countdown implemented
    Few more improvements needed till done.

--- Pomodoro Clock/script.js
+++ Pomodoro Clock/script.js
@@ -4,8 +4,51 @@ var minus = document.querySelector(".brln .minus");
 var ssLen = document.querySelector(".ssln .length");
 var ssPlus = document.querySelector(".ssln .plus");
 var ssMinus = document.querySelector(".ssln .minus");
+var minutes = document.querySelector("#minutes");
+var seconds = document.querySelector("#seconds");
+var timer = document.querySelector(".timer");
 var numLength = Number(len.innerHTML);
 var ssNumLength = Number(ssLen.innerHTML);
+var br = 0;
+var ss = 1;
+
+minutes.innerHTML = ssNumLength;
+
+timer.addEventListener("click", function() {
+  minutes.innerHTML -= 1;
+  seconds.innerHTML = 59;
+  setInterval(function() {
+    if (seconds.innerHTML > 0) {
+      seconds.innerHTML -= 1;
+    } else if (minutes.innerHTML == 0 && seconds.innerHTML == 0) {
+        if (br < ss) {
+          minutes.innerHTML = numLength - 1;
+          seconds.innerHTML = 59;
+          br ++;
+        } else if (br == ss) {
+          minutes.innerHTML = ssNumLength - 1;
+          seconds.innerHTML = 59;
+          ss ++;
+        }
+        console.log(br);
+    } else if (seconds.innerHTML == 0) {
+        minutes.innerHTML -= 1;
+        seconds.innerHTML = 59;
+    }
+  }, 1000);
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 plus.addEventListener("click", function() {
   numLength += 1;
@@ -22,11 +65,13 @@ minus.addEventListener("click", function() {
 ssPlus.addEventListener("click", function() {
   ssNumLength += 1;
   ssLen.innerHTML = ssNumLength;
+  countdown.innerHTML = ssNumLength;
 });
 
 ssMinus.addEventListener("click", function() {
   if (ssLen.innerHTML > 1) {
     ssNumLength -= 1;
     ssLen.innerHTML = ssNumLength;
+    countdown.innerHTML = ssNumLength;
   }
 });
