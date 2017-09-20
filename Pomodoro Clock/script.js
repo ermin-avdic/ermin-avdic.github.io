452e2bf1fe764c7e300dac0a4322d93cb9106b32
Ermin <usb007agent@gmail.com>
2017-09-20 17:22:00 +0200

    Pomodoro is fully functional now
    May add better stylings in future.

--- Pomodoro Clock/script.js
+++ Pomodoro Clock/script.js
@@ -7,71 +7,67 @@ var ssMinus = document.querySelector(".ssln .minus");
 var minutes = document.querySelector("#minutes");
 var seconds = document.querySelector("#seconds");
 var timer = document.querySelector(".timer");
+var timerp = document.querySelector("#timerp");
 var numLength = Number(len.innerHTML);
 var ssNumLength = Number(ssLen.innerHTML);
 var br = 0;
 var ss = 1;
+var i = 0;
 
 minutes.innerHTML = ssNumLength;
 
 timer.addEventListener("click", function() {
-  minutes.innerHTML -= 1;
-  seconds.innerHTML = 59;
-  setInterval(function() {
-    if (seconds.innerHTML > 0) {
-      seconds.innerHTML -= 1;
-    } else if (minutes.innerHTML == 0 && seconds.innerHTML == 0) {
-        if (br < ss) {
-          minutes.innerHTML = numLength - 1;
+  i++;
+  if (i % 2 != 0) {
+    minutes.innerHTML -= 1;
+    seconds.innerHTML = 59;
+    setInterval(function() {
+      if (seconds.innerHTML > 0) {
+        seconds.innerHTML -= 1;
+      } else if (minutes.innerHTML == 0 && seconds.innerHTML == 0) {
+          if (br < ss) {
+            minutes.innerHTML = numLength - 1;
+            seconds.innerHTML = 59;
+            timerp.textContent = "Break";
+            br++;
+          } else if (br == ss) {
+            minutes.innerHTML = ssNumLength - 1;
+            seconds.innerHTML = 59;
+            timerp.textContent = "Session"
+            ss++;
+          }
+      } else if (seconds.innerHTML == 0) {
+          minutes.innerHTML -= 1;
           seconds.innerHTML = 59;
-          br ++;
-        } else if (br == ss) {
-          minutes.innerHTML = ssNumLength - 1;
-          seconds.innerHTML = 59;
-          ss ++;
-        }
-        console.log(br);
-    } else if (seconds.innerHTML == 0) {
-        minutes.innerHTML -= 1;
-        seconds.innerHTML = 59;
-    }
-  }, 1000);
+      }
+    }, 1000);
+  } else {
+    window.location.reload(true);
+  }
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
 plus.addEventListener("click", function() {
   numLength += 1;
-  len.innerHTML = numLength;
+  len.innerHTML = Number(numLength);
 });
 
 minus.addEventListener("click", function() {
   if (len.innerHTML > 1) {
     numLength -= 1;
-    len.innerHTML = numLength;
+    len.innerHTML = Number(numLength);
   }
 });
 
 ssPlus.addEventListener("click", function() {
   ssNumLength += 1;
   ssLen.innerHTML = ssNumLength;
-  countdown.innerHTML = ssNumLength;
+  minutes.innerHTML = Number(ssNumLength);
 });
 
 ssMinus.addEventListener("click", function() {
   if (ssLen.innerHTML > 1) {
     ssNumLength -= 1;
     ssLen.innerHTML = ssNumLength;
-    countdown.innerHTML = ssNumLength;
+    minutes.innerHTML = Number(ssNumLength);
   }
 });
